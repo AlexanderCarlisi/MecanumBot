a98f3216204dc0b4deaef4be97e38baf09c51733
AlexanderCarlisi <156526739+AlexanderCarlisi@users.noreply.github.com>
2025-03-28 10:22:16 -0400

    Update motor.cpp to new definitions in motor.hpp

--- motor.cpp
+++ motor.cpp
@@ -3,50 +3,50 @@
 #include <Arduino.h>
 
 
-Motor::Motor(uint8_t pinCW, uint8_t pinCCW, uint8_t pinSpeed, bool inverted)
-      : pinCW(pinCW), pinCCW(pinCCW), pinSpeed(pinSpeed), inverted(inverted)
+Motor::Motor(uint8_t dioCW, uint8_t dioCCW, uint8_t pwm, bool inverted)
+      : PIN_DIO_CW(dioCW), PIN_DIO_CCW(dioCCW), PIN_PWM(pwm), inverted(inverted)
 {
   // Setup Pin Modes
-  pinMode(pinCW, OUTPUT);
-  pinMode(pinCCW, OUTPUT);
-  pinMode(pinSpeed, OUTPUT);
+  pinMode(PIN_DIO_CW, OUTPUT);
+  pinMode(PIN_DIO_CCW, OUTPUT);
+  pinMode(PIN_DIO_CCW, OUTPUT);
 }
 
 
 void Motor::setDirection(bool forward) {
   // CCW (+ & FORWARD)
   bool goCCW = forward && !this->inverted;
-  digitalWrite(this->pinCCW, goCCW ? HIGH : LOW);
-  digitalWrite(this->pinCW, goCCW ? LOW : HIGH);
+  digitalWrite(this->PIN_DIO_CCW, goCCW ? HIGH : LOW);
+  digitalWrite(this->PIN_DIO_CW, goCCW ? LOW : HIGH);
 }
 
 
 void Motor::stop() {
-  analogWrite(this->pinSpeed, 0);
+  analogWrite(this->PIN_PWM, 0);
 }
 
 
-void Motor::setPWM(uint8_t speed, bool forward) {
+void Motor::setPWM(uint8_t signal, bool forward) {
   if (speed != 0) this->setDirection(forward);
 
   // Set motor speed
-  analogWrite(this->pinSpeed, speed);
+  analogWrite(this->PIN_PWM, signal);
 }
 
 
-void Motor::setPWM(uint16_t speed) {
+void Motor::setPWM(uint16_t signal) {
   if (speed == 0) this->stop();
   else {
-    bool forward = speed > 0;
-    this->setPWM((forward) ? speed : speed * -1, forward);
+    bool forward = signal > 0;
+    this->setPWM((forward) ? signal : signal * -1, forward);
   }
 }
 
 
-void Motor::set(float power) {
+void Motor::set(float dutyCycle) {
   if (power == 0) this->stop();
   else {
-    power = constrain(power, -MOTOR_MAX_OUPUT, MOTOR_MAX_OUTPUT);
-    this->setPWM((uint8_t)(power * MOTOR_MAX_PWM));
+    power = constrain(dutyCycle, -MOTOR_MAX_OUPUT, MOTOR_MAX_OUTPUT);
+    this->setPWM((uint8_t)(dutyCycle * MOTOR_MAX_PWM));
   }
 }
